21a1a7545ca293432aa3bc426bc8aec8ec890f4d
jarvis-1805 <shubhsahu1805@gmail.com>
2020-09-11 18:44:28 +0530

    Updated FCFS scheduling code

--- FCFS/FCFS.cpp
+++ FCFS/FCFS.cpp
@@ -1,53 +1,90 @@
 #include <iostream>
+#include<iomanip>
 #define MAX 100
+
 using namespace std;
 
 class FCFS
 {
   public:
-    double wt[MAX];
+    string unit;
+    float wt[MAX];
+    float tt[MAX];
     
-    void waitingTime(int, double *, double *);
+    FCFS();
+
+    void waitingTime(int, float *, float *);
     void avgWaitingTime(int);
-    void turnAroundTime(int, double *, double *);
+    void turnAroundTime(int, float *, float *);
     void avgTurnAroundTime(int);
 };
 
-void FCFS::waitingTime(int n, double *bt, double *at)
+FCFS::FCFS()
 {
-  double b=0.0;
-  //b = bt[0];
+  cout << "\nEnter the unit: ";
+  cin >> unit;
+}
+
+void FCFS::waitingTime(int n, float *bt, float *at)
+{
+  float b=0.0;
   wt[0] = at[0];
   for(int i=0; i<n; i++)
   {
     b = b + bt[i];
     wt[i+1] = b - at[i+1];
   }
-  for(int j=0; j<n; j++)
-  {
-    cout << "P" << j+1 << " =     " << at[j] << "\t\t" << bt[j] << "\t\t" << wt[j] << "\n";
-  }
 }
 
 void FCFS::avgWaitingTime(int n)
 {
-  double twt = 0, awt;
+  float twt = 0, awt;
   for(int i=0; i<n; i++)
   {
     twt = twt + wt[i];
   }
   awt = twt/n;
-  cout << awt;
+  cout << awt << unit;
+}
+
+void FCFS::turnAroundTime(int n, float *bt, float *at)
+{
+  for(int i=0; i<n; i++)
+  {
+    tt[i] = wt[i] + bt[i];
+  }
+  for(int j=0; j<n; j++)
+  {
+    cout << fixed << setprecision(2);
+    cout << setw(5) << j+1 
+        << setw(15) << at[j] << unit
+        << setw(15) << bt[j] << unit
+        << setw(15) << wt[j] << unit
+        << setw(15) << tt[j] << unit << "\n";
+  }
+}
+
+void FCFS::avgTurnAroundTime(int n)
+{
+  float ttt = 0, att;
+  for(int i=0; i<n; i++)
+  {
+    ttt = ttt + tt[i];
+  }
+  att = ttt/n;
+  cout << att << unit;
 }
 
 int main()
 {
   FCFS ob;
+
   int n;
   cout << "\nEnter the no. of processes : ";
   cin >> n;
   cout << endl;
-  double bt[n], at[n];
+
+  float bt[n], at[n];
   for(int i=0; i<n; i++)
   {
     cout << "Enter the arrival time of process " << i+1 << " : ";
@@ -55,10 +92,16 @@ int main()
     cout << "Enter the burst time of process " << i+1 << " : ";
     cin >> bt[i];
     cout << endl;
-  }  
-  cout << "\n\tArrival Time\tBurst Time\tWaiting Time\n";
+  }
+
+  cout << "\nProcess Id\t\tArrival Time\tBurst Time\t\tWaiting Time\tTurn Around Time\n";
   ob.waitingTime(n, bt, at);
-  cout << "\nAverage Waiting TIme: ";
+  ob.turnAroundTime(n, bt, at);
+
+  cout << "\nAverage Waiting Time: ";
   ob.avgWaitingTime(n);
+
+  cout << "\nAverage Turn Around Time: ";
+  ob.avgTurnAroundTime(n);
   cout << endl;
 }
