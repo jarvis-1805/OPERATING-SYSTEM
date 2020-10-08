56582ed3fbc68a35f7dfe93e48bd25707171c4a0
jarvis-1805 <shubhsahu1805@gmail.com>
2020-10-08 22:35:24 +0530

    Added SJF Schedulng Algorithm

--- SJF/SJF.cpp
+++ SJF/SJF.cpp
@@ -6,131 +6,115 @@ using namespace std;
 
 class SJF
 {
-  public:
-    string unit;
-    float wt[MAX];
-    float tt[MAX];
-    float bt[MAX];
-    float at[MAX];
-    int process[MAX];
+	public:
+		string unit;
+		int pid[MAX];
+		float bt[MAX];
+		float at[MAX];
+		float ct[MAX];
+		float wt[MAX];
+		float tt[MAX];
+		float completed[MAX];
+		int start, total;
     
-    SJF();
+		SJF();
 
-    void sort(int);
+		void calculate(int);
 
-    void waitingTime(int);
-    void avgWaitingTime(int);
-    void turnAroundTime(int);
-    void avgTurnAroundTime(int);
+		void avg_waiting_time(int);
+		void avg_turn_around_time(int);
 };
 
 SJF::SJF()
 {
-  cout << "\nEnter the unit: ";
-  cin >> unit;
+	start = 0;
+	total = 0;
+	cout << "\nEnter the unit: ";
+	cin >> unit;
 }
 
-void SJF::sort(int n)
+void SJF::calculate(int n)
 {
-  float temp, temp1, temp2;
-  for(int i=0; i<n; i++)
-  {
-    for(int j=0; j<n-i-1; j++)
-    {
-      if(at[j] > at[j+1])
-      {
-        temp = bt[j];
-        bt[j] = bt[j+1];
-        bt[j+1] = temp;
-
-        temp1 = process[j];
-        process[j] = process[j+1];
-        process[j+1] = temp1;
-
-        temp2 = at[j];
-        at[j] = at[j+1];
-        at[j+1] = temp2;
-      }
-    }
-  }
+	while(total != n)
+	{
+		int c=n, minim=999;
+		for(int i=0; i<n; i++)
+		{
+			if(at[i] <= start && completed[i] != 1 && bt[i] < minim)
+			{
+				minim = bt[i];
+				c = i;
+			}
+		}
+		if(c == n)
+			start++;
+		else
+		{
+		  ct[c] = start + bt[c];
+		  start += bt[c];
+		  tt[c] = ct[c] - at[c];
+		  wt[c] = tt[c] - bt[c];
+		  completed[c] = 1;
+		  pid[total] = c + 1;
+		  total++;
+		}
+	}
 }
 
-void  SJF::waitingTime(int n)
+void  SJF::avg_waiting_time(int n)
 {
-  float b=0.0;
-  wt[0] = at[0];
-  for(int i=0; i<n; i++)
-  {
-    b = b + bt[i];
-    wt[i+1] = b - at[i+1];
-  }
+	float twt = 0, awt;
+	for(int i=0; i<n; i++)
+		twt = twt + wt[i];
+	awt = twt/n;
+	cout << awt << unit;
 }
 
-void  SJF::avgWaitingTime(int n)
+void  SJF::avg_turn_around_time(int n)
 {
-  float twt = 0, awt;
-  for(int i=0; i<n; i++)
-  {
-    twt = twt + wt[i];
-  }
-  awt = twt/n;
-  cout << awt << unit;
-}
-
-void  SJF::turnAroundTime(int n)
-{
-  for(int i=0; i<n; i++)
-  {
-    tt[i] = wt[i] + bt[i];
-  }
-  for(int j=0; j<n; j++)
-  {
-    cout << fixed << setprecision(2);
-    cout << setw(5) << process[j]
-        << setw(15) << at[j] << unit
-        << setw(15) << bt[j] << unit
-        << setw(15) << wt[j] << unit
-        << setw(15) << tt[j] << unit << "\n";
-  }
-}
-
-void  SJF::avgTurnAroundTime(int n)
-{
-  float ttt = 0, att;
-  for(int i=0; i<n; i++)
-  {
-    ttt = ttt + tt[i];
-  }
-  att = ttt/n;
-  cout << att << unit;
+	float ttt = 0, att;
+	for(int i=0; i<n; i++)
+		ttt = ttt + tt[i];
+	att = ttt/n;
+	cout << att << unit;
 }
 
 int main()
 {
-  cout << "========== SJF scheduling algorithm ==========";
-  SJF ob;
-  int n;
-  cout << "\nEnter the no. of processes : ";
-  cin >> n;
-  cout << endl;
-  
-  for(int i=0; i<n; i++)
-  {
-    ob.process[i] = i+1;
-    cout << "Enter the arrival time of process " << i+1 << " : ";
-    cin >> ob.at[i];
-    cout << "Enter the burst time of process " << i+1 << " : ";
-    cin >> ob.bt[i];
-    cout << endl;
-  }
+	cout << "========== SJF scheduling algorithm ==========";
+	SJF ob;
+	int n;
+	cout << "\nEnter the no. of processes : ";
+	cin >> n;
+	cout << endl;
 
-  ob.sort(n);
-  cout << "\nProcess Id\tArrival Time\tBurst Time\tWaiting Time\tTurn Around Time\n";
-  ob.waitingTime(n);
-  ob.turnAroundTime(n);
-  cout << "\nAverage Waiting Time: ";
-  ob.avgWaitingTime(n);
-  cout << "\nAverage Turn Around Time: ";
-  ob.avgTurnAroundTime(n);
-  cout << endl;
+	for(int i=0; i<n; i++)
+	{
+		cout << "Process " << i+1 << endl;
+		cout << "Enter the arrival time : ";
+		cin >> ob.at[i];
+		cout << "Enter the burst time : ";
+		cin >> ob.bt[i];
+		ob.pid[i] = i+1;
+		ob.completed[i] = 0;
+		cout << endl;
+	}
+
+	ob.calculate(n);
+	cout << "\nProcess Id\tArrival Time\tBurst Time\tCompletetion Time\tWaiting Time\tTurn Around Time\n";
+	for(int j=0; j<n; j++)
+	{
+		cout << fixed << setprecision(2);
+		cout << setw(5) << ob.pid[j]
+			<< setw(12) << ob.at[j] << ob.unit
+			<< setw(15) << ob.bt[j] << ob.unit
+			<< setw(15) << ob.ct[j] << ob.unit
+			<< setw(15) << ob.wt[j] << ob.unit
+			<< setw(15) << ob.tt[j] << ob.unit << "\n";
+	}
+	cout << "\nAverage Waiting Time: ";
+	ob.avg_waiting_time(n);
+	cout << "\nAverage Turn Around Time: ";
+	ob.avg_turn_around_time(n);
+	cout << endl;
 }
